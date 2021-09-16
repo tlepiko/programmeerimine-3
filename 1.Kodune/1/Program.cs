e4d258dc73b45ae5fcecdbdd609dca9f3e814f8a
tlepiko <60349843+tlepiko@users.noreply.github.com>
2021-09-16 13:21:40 +0300

    1.Kodune 1.Ülesanne
    Massiivi tagurpidi kirjutamine.

---
+++ 1.Kodune/1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+int[] array1 = new int[] { 1, 2, 3, 4, 5 };
+string array1String = "";
+foreach(var arrayElement in array1)
+{
+    array1String = array1String + arrayElement;
+}
+int length = array1.Length - 1;
+string array1Reverse = null;
+while (length >= 0)
+{
+    array1Reverse = array1Reverse + array1[length];
+    length--;
+}
+Console.WriteLine();
+Console.WriteLine("Algne massiiv on: " + array1String);
+Console.WriteLine();
+Console.WriteLine("Massiiv tagurpidi on: " + array1Reverse);
+Console.ReadLine();
